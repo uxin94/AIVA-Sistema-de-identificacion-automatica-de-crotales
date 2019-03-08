4bc9e88a3210368e57667a2986e268d7e369dd4c
uxin94 <47525138+uxin94@users.noreply.github.com>
2019-03-08 12:54:06 +0100

    Add files via upload

---
+++ roi.py
@@ -0,0 +1,13 @@
+class roi():
+
+    def __init__(self, y0, x0, y1, x1):
+
+        # Coord. sup. izq.
+        self.x0 = x0
+        self.y0 = y0
+        # Coord. inf. dcha.
+        self.x1 = x1
+        self.y1 = y1
+
+
+
--- test_numeros.py
+++ test_numeros.py
@@ -1,26 +1,52 @@
 import unittest
 from unittest import TestCase
+import random
 from Crotales.numeros import numeros
+from Crotales.mock_up import readnum
 
 
 class TestReadnum(TestCase):
 
     def test_numeros_iguales(self):
 
-        num1 = numeros(1, 2, 3, 4)
-        num2 = numeros(1, 2, 3, 4)
-        self.assertEqual(num1.a, num2.a)
-        self.assertEqual(num1.b, num2.b)
-        self.assertEqual(num1.c, num2.c)
-        self.assertEqual(num1.d, num2.d)
+        num1 = numeros(1, 2, 3, 4, 5)
+        n = random.randint(4, 5)
+        num2 = readnum(n)
+
+        if n == 4:
+            #llamar a la funcion que "lee los numeros"
+            self.assertEqual(num1.a, num2[0])
+            self.assertEqual(num1.b, num2[1])
+            self.assertEqual(num1.c, num2[2])
+            self.assertEqual(num1.d, num2[3])
+        else:
+            self.assertEqual(num1.a, num2[0])
+            self.assertEqual(num1.b, num2[1])
+            self.assertEqual(num1.c, num2[2])
+            self.assertEqual(num1.d, num2[3])
+            self.assertEqual(num1.e, num2[4])
+
 
     def test_numeros_diferentes(self):
-        num1 = numeros(1, 2, 3, 4)
-        num2 = numeros(5, 6, 7, 9)
-        self.assertNotEqual(num1.a, num2.a)
-        self.assertNotEqual(num1.b, num2.b)
-        self.assertNotEqual(num1.c, num2.c)
-        self.assertNotEqual(num1.d, num2.d)
+
+        num1 = numeros(1, 2, 3, 4, 5)
+        n = random.randint(4, 5)
+        num2 = readnum(n)
+
+        if n == 4:
+            #llamar a la funcion que "lee los numeros"
+            self.assertNotEqual(num1.a, num2[0])
+            self.assertNotEqual(num1.b, num2[1])
+            self.assertNotEqual(num1.c, num2[2])
+            self.assertNotEqual(num1.d, num2[3])
+        else:
+            self.assertNotEqual(num1.a, num2[0])
+            self.assertNotEqual(num1.b, num2[1])
+            self.assertNotEqual(num1.c, num2[2])
+            self.assertNotEqual(num1.d, num2[3])
+            self.assertNotEqual(num1.e, num2[4])
+
+
 
 
 if __name__ == '__main__':
