d386d913e1cbe578dc388cfa2445a965c16dd17f
uxin94 <47525138+uxin94@users.noreply.github.com>
2019-03-28 11:18:27 +0100

    Delete prueba.py

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from recognize import Recognizer
-
-
-#A = cv2.imread('/Users/andrea/Desktop/MASTER/SEGUNDO CUATRI/APLICACIONES/Proyecto crotales/CrotalesTest/Imagenes/Test/0002.TIF')
-A = cv2.imread('/Users/andrea/Desktop/MASTER/SEGUNDO CUATRI/APLICACIONES/Proyecto crotales/CrotalesTest/TestSamples/0003.TIF')
-r = Recognizer()
-roi0 = r.recognize(A)
-print(roi0)
